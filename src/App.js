5e7c92d14ce3d867632e3f2f48b020c6479c1b45
Abhishek Yadav <abh5425@gmail.com>
2023-03-09 22:41:14 +0530

    completed....

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import NavBar from './components/NavBar';
 import News from './components/News';
 
 // import {
-//   BrowserRouter as Router,
+//  Router,
 //   Routes,
 //   Route
 // } from "react-router-dom";
